9462b1ecc875e84de2ee318c83fd74a6ae40c35c
Shawn Gaetano <sgaetano@syr.edu>
2019-05-05 14:11:34 -0400

    Added upload template page. Made state pages for feed, upload and profile functional. Used database API to display pictures users have uploaded to the feed page. Uploading of photos now works and has been tested. Small fixes on components to practice clean and effiecient code.

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@
     //Create a module
     let app = angular.module('app', ['ui.router', 'istAuth']);
 
-
+    var token = "14oXeQr2u4ZjVHPLO7OSLIRc8GvjMACjxUjVMc2F"
     //Config Block
     app.config(function($stateProvider, $urlRouterProvider) {
         //Configure the routes for the application
@@ -13,6 +13,34 @@
 				templateUrl: '/templates/index.html',
 				controller: 'indexCtrl'
             })
+
+            .state('feed', {
+                url:'/feed',
+                templateUrl:'/templates/feed.html',
+                controller:'feedCtrl'
+            })
+
+            .state('feedItem', {
+                url:'/feed:id',
+                templateUrl:'/templates/feed-item.html',
+                controller:'feedItemCtrl'
+            })
+
+            .state('upload', {
+                url:'/upload',
+                templateUrl:'/templates/upload.html',
+                controller:'uploadCtrl'
+            })
+
+
+            .state('profile', {
+                url:'/profile/:id',
+                templateUrl:'/templates/profile.html',
+                controller:'profileCtrl',
+
+            
+            })
+
         
         //If the user requests a URL that isn't mapped to a route, redirect them to the homepage
         $urlRouterProvider.otherwise('index');
@@ -47,22 +75,87 @@
     });
 
 
-    app.controller('feedCtrl', function(){
-        //Controller logic here
+    app.controller('feedCtrl', function($scope, $http){
+
+        $scope.images=[];
+
+        $http({
+            url: 'https://exchangeagram.azurewebsites.net/api/media',
+            method: 'GET',
+            params: {
+                token:token,
+                count:13,
+                page:'1',      
+            }
+
+            })
+            .then(function(response) {
+                console.log(response.data.data);
+                $scope.images = response.data.data
+            },
+            function(error) {
+                console.log(error);
+
+        });
     });
 
-    app.controller('feedItemCtrl', function(){
-        //Controller logic here
+    app.controller('feedItemCtrl', function($scope, $http){
+
     });
 
-    app.controller('uploadCtrl', function(){
-        //Controller logic here
+    app.controller('uploadCtrl', function($scope, $rootScope){
+        let url = $window.location.href;
+
+        if (url.includes('success=') && url.includes('id=')) { 
+            let url_success = url.split('success=')[1].split('&')[0]
+            let url_id = url.split('id=')[1].split('&')[0];
+
+            $state.go('feedItem',{id:url_id});
+        }
+        
     });
 
-    app.controller('profileCtrl', function(){
-        //Controller logic here
+    app.controller('profileCtrl', function($scope, $http) {
+        $http({
+            url: 'https://exchangeagram.azurewebsites.net/api/media',
+            method: 'POST',
+            params: {
+                token:token,
+                id:'',
+            }
+
+            })
+            .then(function(response) {
+                console.log(response.data);
+            },
+            function(error) {
+                console.log(error);
+
+        });
+
     });
 
+
+    app.controller('profileEditCtrl',function(){
+        $http({
+            url: 'https://exchangeagram.azurewebsites.net/api/media',
+            method: 'POST',
+            params: {
+                token:token,
+                username:'',
+                bio:'',      
+            }
+
+            })
+            .then(function(response) {
+                console.log(response.data);
+            },
+            function(error) {
+                console.log(error);
+
+        });
+    
+    });
 
 
     app.controller('mainCtrl', function($scope, $auth, $rootScope) {
@@ -96,36 +189,6 @@
     app.component('postPreview', {
         templateUrl:'/templates/post-preview.html', 
         controller: 'indexCtrl'
-
-    })
-
-    app.component('index', {
-        templateUrl:'/templates/index.html', 
-        controller: 'indexCtrl'
-
-    })
-
-    app.component('feed', {
-        templateUrl:'/templates/feed.html', 
-        controller: 'mainCtrl'
-
-    })
-
-    app.component('feedItem', {
-        templateUrl:'/templates/feed-item.html', 
-        controller: 'mainCtrl'
-
-    })
-
-    app.component('upload', {
-        templateUrl:'/templates/upload.html', 
-        controller: 'mainCtrl'
-
-    })
-
-    app.component('profile', {
-        templateUrl:'/templates/profile.html', 
-        controller: 'mainCtrl'
 
     })
 
